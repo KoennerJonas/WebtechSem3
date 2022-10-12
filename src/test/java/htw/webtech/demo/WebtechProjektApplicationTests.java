aef7856be084f14ae60739f4df6d44c049b85c1e
j k <Jonas.Koenner@Student.HTW-Berlin.de>
2022-10-12 14:41:14 +0200

    initializr

---
+++ src/test/java/htw/webtech/demo/WebtechProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.webtech.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
